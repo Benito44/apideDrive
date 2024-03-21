bdabc35a70ced32722a50b16746d401f5590348a
MarioFlores44 <m.flores2@sapalomera.cat>
2024-03-21 17:03:23 +0100

    Acceder al contenido del .epub

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 npm install multer
 npm install googleapis
+npm install adm-zip
+
+## PROGRESO
+Mario, 21/03/2024 --> Hasta ahora, podemos subir archivos, descomprimirlos y coger el contenido de la carpeta de contenido y crear los links para ver los capítulos.
+Se envian bien los links, y se crea la ventana nueva (no como emergente, eso hay que arreglarlo), pero no se ve el contenido, falta de solucionar este problema.
--- servidors.js
+++ servidors.js
@@ -5,6 +5,9 @@ import { google } from 'googleapis';
 import multer from 'multer';
 import { fileURLToPath } from 'url';
 import { GoogleDriveClient } from './drive.js';
+import AdmZip from 'adm-zip';
+
+
 
 const app = express();
 const PORT = 8080;
@@ -32,6 +35,8 @@ const __dirname = dirname(__filename);
 
 let libros = [];
 
+app.use('/books', express.static(path.join(__dirname, 'books')));
+
 // Luego, puedes usar __dirname en tu ruta:
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
@@ -54,6 +59,45 @@ app.get('/libroId', async (req, res) => {
     } catch (error) {
         console.error('Error al obtener el libro:', error);
         res.status(500).send('Error al obtener el libro');
+    }
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    if (!req.file) {
+        console.log('No se envió ningún archivo');
+        res.status(400).send('No se envió ningún archivo');
+    } else {
+        const filePath = req.file.path;
+        const originalName = path.basename(req.file.originalname, '.epub');
+        const targetPath = path.join('books', originalName);
+
+        if (!fs.existsSync(targetPath)) {
+            let zip = new AdmZip(filePath);
+            zip.extractAllTo(targetPath, true);
+        }
+
+        const textDirPath = path.join(targetPath, 'OEBPS', 'Text'); // Ajustado para incluir 'OEBPS'
+        if (!fs.existsSync(textDirPath)) {
+            console.log('No se encontró el directorio Text/');
+            res.status(400).send('No se encontró el directorio Text/');
+        } else {
+            const textFiles = fs.readdirSync(textDirPath);
+            const textLinks = textFiles.map(file => `http://localhost:3000/books/${originalName}/OEBPS/Text/${file}`); // Ajustado para incluir 'OEBPS'
+
+            res.send(textLinks);
+        }
+    }
+});
+
+app.get('/books/:bookId', (req, res) => {
+    const bookId = req.params.bookId;
+    const bookPath = path.resolve('books', bookId);
+
+    if (fs.existsSync(bookPath)) {
+        console.log("libro encontrado")
+        res.send(bookPath);
+    } else {
+        res.status(404).send('Libro no encontrado');
     }
 });
 
