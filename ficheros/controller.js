1c0f2b42a4e7558aadb2dbc5fe630846959d33a8
BenitoMF <b.martinez2@sapalomera.cat>
2024-04-02 21:50:09 +0300

    Canvios de interficie

--- ficheros/controller.js
+++
@@ -1,9 +0,0 @@
-import { GoogleDriveClient } from "./servidor.js";
-
-const credentialsPath = 'google_drive.json';
-const driveClient = new GoogleDriveClient(credentialsPath);
-
-const id = '1tj58NJSDDjqP8u4YR1cN0AV0MFRz7t-Z';
-function uploadFile(file, id) {
-  driveClient.uploadFile(file, id);
-}
